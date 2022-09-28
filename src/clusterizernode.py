35f5daa05fdbeb078c4e210a7a217d3c65210729
Samuel Jones <spjones329@gmail.com>
2022-09-28 12:45:22 -0500

    Added imports for opencv and cv_bridge and fixed kmeans algo to interpret values from source image which is is OpenCV 32 bit floating point format

--- src/clusterizernode.py
+++ src/clusterizernode.py
@@ -1,39 +1,66 @@
+#!/usr/bin/python
+
 import rospy
 
 import numpy as np
-import typing
-from random import randint
+from random import uniform
 import math
-from rospy import numpy_msg
+from cv_bridge import CvBridge
+import cv2
 
 from sensor_msgs.msg import Image
 
+def isclose(a, b, rel_tol=1e-09, abs_tol=1e-03):
+  if a is float('nan') or b is float('nan'): return False
+  abs_a = abs(a)
+  abs_b = abs(b)
+  if (a == float('inf') or a == float('-inf')) \
+      and (b == float('inf') or b == float('-inf')):
+    return True
+  if a == 0:
+    return abs_b < rel_tol and abs_b < abs_tol
+  if b == 0: 
+    return abs_a < rel_tol and abs_a < abs_tol
+  abs_ab = abs(a - b)
+  return abs_ab / min(a, b) < rel_tol and abs_ab < abs_tol
+
 class ClusterizerNode(object):
 
-  MAX_ITER = 5000
+  MAX_ITER = 50
 
   def __init__(self):
     self.pub_cluster_img = rospy.Publisher("/stereo_kmeans/cluster_image", Image, queue_size=10)
-    self.sub_points = rospy.Subscriber("/zedm/zed_node/depth/depth_registered", numpy_msg(Image), queue_size=10)
+    self.sub_points = rospy.Subscriber("/zedm/zed_node/depth/depth_registered", Image, callback=self.points_callback, queue_size=10)
+    self.cvBridge = CvBridge()
 
-  def points_callback(self, msg: Image):
-    pub_msg = Image()
+  def points_callback(self, msg):
+    # type:(Image) -> None
+    print("image received")
+    # cvimg = self.cvBridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    # cvimg = cv2.patchNaNs(cvimg)
+    print(msg.encoding, msg.height, msg.width, msg.step)  
 
     #convert to 2d array of pixels
     h = msg.height #rows
     w = msg.width #cols
-    row_len_bytes = msg.step
-    img = np.frombuffer(msg.data, dtype=np.uint8) # i think disparity pixels are 8-bit greyscale
-    img: np.ndarray[typing.Any, np.dtype[np.uint8]] = img.reshape((h,w))
+    img = np.frombuffer(msg.data, dtype=np.float32) # disparity pixels are 32bit floats
+    img = img.reshape((h,w)) # type: np.ndarray[any, np.dtype[np.uint8]]
 
     # get depth range
-    min_d = min(img)
-    max_d = max(img)
+    min_d = float('inf')
+    max_d = 0
+    for i in range(h):
+      for j in range(w):
+        d = img[i, j]
+        if d < min_d and d > 0: min_d = d
+        if d > max_d and d != float('inf'): max_d = d
+
+    print('min', min_d, 'max', max_d)
 
     # initial centroid selection
     last_h = h-1
     last_w = w-1
-    c1, c2, c3 = randint(min_d, max_d), randint(min_d, max_d), randint(min_d, max_d)
+    c1, c2, c3 = uniform(min_d, max_d), uniform(min_d, max_d), uniform(min_d, max_d)
 
     # k-means
     success = False
@@ -43,6 +70,7 @@ class ClusterizerNode(object):
       for r in range(h): #rows
         for c in range(w): #cols
           depth = img[r, c]
+          if isclose(depth, 0, rel_tol=1e-5) or abs(depth) == float('inf'): continue
           diff1 = abs(depth-c1)
           diff2 = abs(depth-c2)
           diff3 = abs(depth-c3)
@@ -60,25 +88,24 @@ class ClusterizerNode(object):
       newc1 = sum(s1) / len(s1)
       newc2 = sum(s2) / len(s2)
       newc3 = sum(s3) / len(s3)
-      if math.isclose(newc1, c1, rel_tol=1e-5)\
-          and math.isclose(newc2, c2, rel_tol=1e-5)\
-          and math.isclose(newc3, c3, rel_tol=1e-5):
+      if isclose(newc1, c1, rel_tol=1e-5)\
+          and isclose(newc2, c2, rel_tol=1e-5)\
+          and isclose(newc3, c3, rel_tol=1e-5):
         success = True
         break
       c1, c2, c3 = newc1, newc2, newc3
-        
+
+    print("Iterations done: %d" % i)    
     if not success:
       rospy.logerr("Max iterations reached on k-means")
     
-    newimg: np.ndarray = img.copy()
+    newimg = img.copy() # type: np.ndarray
     for d, pts in [(c1, s1pts), (c2, s2pts), (c3, s3pts)]:
       for pt in pts:
         newimg[pt] = d
 
-    pub_msg.data = newimg #check this
-    pub_msg.height = h
-    pub_msg.width = w
-    self.pub_cluster_img.publish(pub_msg)
+    print("publishing")
+    self.pub_cluster_img.publish(self.cvBridge.cv2_to_imgmsg(newimg, encoding="passthrough"))
 
 if __name__ == '__main__':
 
