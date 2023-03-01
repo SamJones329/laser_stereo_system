7c15e5f62a1bfa1fae0c9f48978c962032d080b6
Samuel Jones <spjones329@gmail.com>
2023-02-28 18:22:51 -0600

    Did some pipeline fixes and determined there seems to be some kind of issue with the OpenCV Hough Lines functions. Might have to explicitly provide a destination array for it to function properly with this version.

--- src/laser_detector.py
+++ src/laser_detector.py
@@ -11,7 +11,7 @@ import cupy
 
 # Constants, should move these to param yaml file if gonna use with ROS
 WINLEN = 5 # works for 1080p and 2.2k for Zed mini
-GVAL_MAX_VAL = -2000.
+GVAL_MIN_VAL = 2000.
 
 gpu = cuda.get_current_device()
 maxthreadsperblock2d = math.floor(math.sqrt(gpu.MAX_THREADS_PER_BLOCK))
@@ -23,7 +23,11 @@ def timeit(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        printStr = f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds'
+        if len(printStr) > 100:
+            printStr = f'Function {func.__name__} Took {total_time:.4f} seconds'
+        print(printStr)
+        print()
         return result
     return timeit_wrapper
 
@@ -85,52 +89,21 @@ def cpu_gvals(img):
             gvals.append((col, winstart+WINLEN//2, G))
     return gvals
 
-def generate_candidate_laser_pt_img(img):
-    '''Takes a greyscale laser reward image and thresholds it to a binary image
-
-    :param img: MxN greyscale cv.Mat image or otherwise compatible arraylike with dtype=np.float32
-
-    :return: MxN binary cv.Mat image or otherwise compatible arraylike
+def calculate_gaussian_integral_windows(img):
+    '''Calculates discretized Gaussian integral over window 
+    of size WINLEN. Takes in a mono laser intensity image. 
+    The resulting values can be used with a tuned threshold 
+    value to be considered as laser points in an image.
     ''' 
 
-    # @cuda.jit
-    # def integrate(colvec, out):
-    #     idx = cuda.grid(1)
-    #     out[idx] = np.sum(colvec[idx:idx+WINLEN])
-
-    # GPU for col loop and row loop
-
-    # GPU for col loop with memoization
-
-    # GPU for row loop
-    # gvals = cp.zeros(img.shape)
-    # we want a thread per element
-    # block/thread strategy [WIP]
-    # - start with one block, do up to 256 threads, 
-    # - then increment num blocks until fill both with 256 threads 
-    # - and so on until we reach 16 blocks, 
-    # - at which point we start incrementing thread count up to 1024
-    # - this gives us up to 16,384 threads
-    # - 2.2k mono image has 2,742,336 elements/pixels
-    # - make each thread do multiple?
-
-    # POT improvements 
-    # - idk
-    # - set minimum number of elements below which CPU will be used for less overhead
-    # nbr_thread_per_block = 256 # modern nvidia GPUs have 1024 threads per block max, often standard of 256 is used for convenience
-    # nbr_block_per_grid = 2 # jetson nano has 32 blocks max
+    #memoization?
         
-    # for col in range(cols):
-    #     d_col = cuda.to_device(img[:,col])
-    #     d_out = cuda.device_array(rows)
-    #     integrate[nbr_block_per_grid, nbr_thread_per_block](d_col, d_out)
-        # need to compare with memoization
     threadsperblock = (maxthreadsperblock2d, maxthreadsperblock2d)# (32,32) # thread dims multiplied must not exceed max threads per block
     blockspergrid_x = int(math.ceil(img.shape[0] / threadsperblock[0]))
     blockspergrid_y = int(math.ceil(img.shape[1] / threadsperblock[1]))
     blockspergrid = (blockspergrid_x, blockspergrid_y)
-    print(f"\nBlockDimX = {blockspergrid[0]} \nBlockDimY = {blockspergrid[1]} \nthreadsPerBlock = {threadsperblock}\n")
-    print(f"Image shape: {img.shape}\n")
+    # print(f"\nBlockDimX = {blockspergrid[0]} \nBlockDimY = {blockspergrid[1]} \nthreadsPerBlock = {threadsperblock}\n")
+    # print(f"Image shape: {img.shape}\n")
 
     start_time = time.perf_counter()
     d_img = cuda.to_device(img)
@@ -139,7 +112,7 @@ def generate_candidate_laser_pt_img(img):
     output = output_global_mem.copy_to_host()
     end_time = time.perf_counter()
     total_time = end_time - start_time
-    print(output)
+    # print(output)
     print(f'\nGPU gval gen took {total_time:.4f} seconds')
 
     # start_time = time.perf_counter()
@@ -148,44 +121,38 @@ def generate_candidate_laser_pt_img(img):
     # total_time = end_time - start_time
     # print(f'CPU gval gen took {total_time:.4f} seconds')    
 
-    # threshold
-    # can use this to help choose threshold
-    # expectedgoodgvals = int(img.shape[0] * 15 * 1.6)#1.4) # room for plenty of outliers
-    # gvals = np.sort(output.flatten())
-    # print(f"Gvals Stats \n\tmean: {np.average(gvals)} \n\tmedian: {np.median(gvals)} \n\tmin: {np.min(gvals)} \n\tmax:{np.max(gvals)}")
-    # goodgvals = gvals[:expectedgoodgvals]
-    # print(f"Good Gvals Stats \n\tmean: {np.average(goodgvals)} \n\tmedian: {np.median(goodgvals)} \n\tmin: {np.min(goodgvals)} \n\tmax:{np.max(goodgvals)}")
-    # output = output < GVAL_MAX_VAL * output #use cp array here?
-    return output
+    return output * -1 # i have no idea why they are negative but they should be positive so...
 
 @timeit
-def threshold_gvals(gval_img):
-    return gval_img < GVAL_MAX_VAL * gval_img
-
-@timeit
-def find_gval_subpixels(goodgvals):
-    subpixel_offsets = np.zeros(goodgvals.shape)
+def find_gval_subpixels(gvals, reward_img):
+    if gvals.shape != reward_img.shape: raise Exception("gval array should be same size as reward_img (gval.shape != reward_img.shape)")
+    subpixel_offsets = np.zeros(gvals.shape)
     offset_from_winstart_to_center = WINLEN // 2
 
-    for row in goodgvals:
-        for col in goodgvals:
+    ln_reward = np.log(reward_img)
+    for row in range(gvals.shape[0]-WINLEN//2):
+        for col in range(gvals.shape[1]-WINLEN//2):
+            if gvals[row,col] < GVAL_MIN_VAL: continue
             # center of window
             center = row + offset_from_winstart_to_center
             
-            # f(x), f(x-1), f(x+1)
-            fx = goodgvals[center, col]
-            fxm = goodgvals[center-1, col]
-            fxp = goodgvals[center+1, col]
-            denom = math.log(fxm) - 2 * math.log(fx) + math.log(fxp)
+            # ln(f(x)), ln(f(x-1)), ln(f(x+1))
+            lnfx = ln_reward[center, col]
+            lnfxm = ln_reward[center-1, col]
+            lnfxp = ln_reward[center+1, col]
+            denom = lnfxm - 2 * lnfx + lnfxp
             if denom == 0:
                 # 5px Center of Mass (CoM5) detector
-                fxp2 = goodgvals[center+2, col] # f(x+2)
-                fxm2 = goodgvals[center-2, col] # f(x-2)
+                fx = reward_img[center, col] # f(x)
+                fxp = reward_img[center+1, col] # f(x+1)
+                fxm = reward_img[center-1, col] # f(x-1)
+                fxp2 = reward_img[center+2, col] # f(x+2)
+                fxm2 = reward_img[center-2, col] # f(x-2)
                 num = 2*fxp2 + fxp - fxm - 2*fxm2
                 denom = fxm2 + fxm + fx + fxp + fxp2
                 subpixel_offset = num / denom
             else:
-                numer = math.log(fxm) - math.log(fxp)
+                numer = lnfxm - lnfxp
                 subpixel_offset = 0.5 * numer / denom
             subpixel_offsets[center, col] = subpixel_offset
     return subpixel_offsets
@@ -195,8 +162,8 @@ def throw_out_small_patches(gval_subpixels):
     laser_patch_img = np.copy(gval_subpixels)
     patches = []
     # find patches
-    for row in range(laser_patch_img.shape[0]):
-        for col in range(laser_patch_img.shape[1]):
+    for row in range(gval_subpixels.shape[0]):
+        for col in range(gval_subpixels.shape[1]):
             val = laser_patch_img[row,col]
             if val > 1e-6: # found laser px, look for patch
                 patch = [(row,col)]
@@ -218,7 +185,8 @@ SEGMENT_MAX_SPAN_TREE = 1
 @timeit
 def segment_laser_lines(img, segment_mode):
     if segment_mode == SEGMENT_HOUGH_LINES_P:
-        cv.HoughLinesP(img, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=5)
+        return cv.HoughLines(img, 1, np.pi / 180, threshold=100, srn=2, stn=2)
+        return cv.HoughLinesP(img, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=5)
     elif segment_mode == SEGMENT_MAX_SPAN_TREE:
         # Let 15 parallel lines be defined as 15 groups of indexes or labels k = {1, 2, . . . , 25}. Let
         # Pl = {p1, p2, . . . , pm} a group of pixels who share at least one corner. We define pa a
@@ -312,7 +280,7 @@ if __name__ == "__main__":
     cpimgs = []
     filenames = []
     for filename in os.listdir(img_folder):
-        print(f"opening ${filename}")
+        print(f"opening {filename}")
         img = Image.open(os.path.join(img_folder, filename))
         if img is not None:
             imgs.append(np.asarray(img))
@@ -320,32 +288,64 @@ if __name__ == "__main__":
             filenames.append(filename)
     print("found images: %s\n" % filenames)
 
-    numpyrewardtimes = 0
-    for img in imgs:
-        start_time = time.perf_counter()
-        color_weights = (0.08, 0.85, 0.2)
-        reward = np.sum(img * color_weights, axis=2)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        numpyrewardtimes += total_time
-        print(f'\nNumpy reward img took {total_time:.4f} seconds')
+    # numpyrewardtimes = 0
+    # for img in imgs:
+    #     start_time = time.perf_counter()
+    #     color_weights = (0.08, 0.85, 0.2)
+    #     reward = np.sum(img * color_weights, axis=2)
+    #     end_time = time.perf_counter()
+    #     total_time = end_time - start_time
+    #     numpyrewardtimes += total_time
+    #     print(f'\nNumpy reward img took {total_time:.4f} seconds')
 
-        gvals = generate_candidate_laser_pt_img(reward)
-        goodgvals = threshold_gvals(gvals)
+    #     gvals = generate_candidate_laser_pt_img(reward)
+    #     goodgvals = threshold_gvals(gvals)
     
     cupyrewardtimes = 0
     for img in imgs:
         start_time = time.perf_counter()
         color_weights = (0.08, 0.85, 0.2)
-        reward = cupy.sum(img * color_weights, axis=2)
+        reward = cupy.sum(img * color_weights, axis=2) 
         end_time = time.perf_counter()
         total_time = end_time - start_time
         cupyrewardtimes += total_time
         print(f'Cupy reward img took {total_time:.4f} seconds')
+        gvals = calculate_gaussian_integral_windows(reward)
+        subpxs = find_gval_subpixels(gvals, reward)
+        subpxsfiltered = throw_out_small_patches(subpxs)
+        print(subpxsfiltered)
+        laserpxbinary = np.zeros(subpxsfiltered.shape, dtype=np.uint8)
+        cv.threshold(subpxsfiltered, 1e-6, 255, type=cv.THRESH_BINARY, dst=laserpxbinary)
+        print(f"laserpxbinary {type(laserpxbinary)} {laserpxbinary}")
+        # laserpxbinary = subpxsfiltered.copy()
+        # laserpxbinary[laserpxbinary > 0] = 1
+        # lines = segment_laser_lines(laserpxbinary, SEGMENT_HOUGH_LINES_P)
+        lines = cv.HoughLines(laserpxbinary, 1, np.pi / 180, threshold=10, srn=2, stn=2)
+        print(lines)
+        lines = cv.HoughLinesP(laserpxbinary, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=5)
+        print(lines)
+        lines = np.reshape(lines, (lines.shape[0], lines.shape[2]))
+        dispimg = img.copy()
+        print("\nLines: ")
+        if lines is not None:
+            lines = lines[lines[:, 0].argsort()] 
+            for i in range(0, len(lines)):
+                print("lines %s" % lines[i])
+                rho = lines[i][0]
+                theta = lines[i][1]
+                a = math.cos(theta)
+                b = math.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 + 2000*(-b)), int(y0 + 2000*(a)))
+                pt2 = (int(x0 - 2000*(-b)), int(y0 - 2000*(a)))
+                cv.line(dispimg, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
 
-        gvals = generate_candidate_laser_pt_img(reward)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
     
-    print(f'\nNumpy reward img avg = {numpyrewardtimes/len(imgs):.4f} seconds')
+    # print(f'\nNumpy reward img avg = {numpyrewardtimes/len(imgs):.4f} seconds')
     print(f'\nCupy reward img avg = {cupyrewardtimes/len(imgs):.4f} seconds')
 
     
